3611becf1f44c134a62df3911c1bb5d5cc903e99
Henry <henryye2006@gmail.com>
2023-02-24 10:01:03 -0800

    started with framework using grid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+//import logo from './images/refresh.png';
 
 function App() {
+  
+  const [count,setCount] = React.useState(0);
+  
+  const handleClick = () => {
+    if (count > 0) {
+      setCount(count => count - 1);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="logo">
+        <div className="items">LOGO</div>
+        <div className="items">Business Name</div>
+        <div className="items">Est. 2020</div>
+      </div>
+      <div className="header">header</div>
+      <div className="nav">nav</div>
+      <div className="content">content</div>
+      <div className="shop">
+        <div className="refresh">REFRESH</div>
+        <div className="Slogo">  
+          <button type="button" onClick={handleClick}>
+            Refresh
+            (Cost:1)
+          </button>
+          {count}
+        </div>
+        <div className="item"></div>
+        <div className="item"></div>
+      </div>
     </div>
   );
 }
