abd75da54c62e74bea4a9387a3d6dcfe5531b7c6
Henry <henryye2006@gmail.com>
2023-03-05 22:51:27 -0800

    bible api added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import './App.css';
-//import logo from './images/refresh.png';
+
 
 function App() {
-  
+  const [verses, setVerses] = useState([]);
+
+  useEffect(() => {
+    fetch('https://www.rkeplin.com/api/verse?reference=john%203:16')
+    .then(response => response.json())
+    .then(data =>setVerses(data))
+    .catch(error => console.error(error));
+  }, []);
   const [count,setCount] = React.useState(5);
   
   const handleClick = () => {
+
+    if(count > 0) {
+      setCount(count => count - 1);
+    }
+
+  };
+  const itemClick1 = () => {
+    if(count > 0) {
+      setCount(count => count - 5);
+    }
+  };
+  const itemClick2 = () => {
     if(count > 0) {
       setCount(count => count - 1);
     }
   };
-  const itemClick = () => {
+  const itemClick3 = () => {
     if(count > 0) {
-      setCount(count => count - 1);
+      setCount(count => count - 3);
+    }
+  };
+  const itemClick4 = () => {
+    if(count > 0) {
+      setCount(count => count - 2);
     }
   };
 
@@ -21,9 +45,15 @@ function App() {
   return (
     <div className="container">
       
-      <div className="header">header</div>
+      <div className="header">
+        {verses.book} {verses.chapter}:{verses.verse}
+        {verses.text}
+      </div>
       
-      <div className="content">content</div>
+      <div className="content">
+      
+
+      </div>
       <div className="shop">
         <div className="refresh">REFRESH</div>
         <div className="Slogo">  
@@ -32,10 +62,14 @@ function App() {
           </button>
           money: {count}
         </div>
-        <div className="item1"type="button" onClick={itemClick}></div>
-        <div className="item2"type="button" onClick={itemClick}></div>
-        <div className="item3"type="button" onClick={itemClick}></div>
-        <div className="item4"type="button" onClick={itemClick}></div>
+        <div className="cost1">Cost:5</div>
+        <div className="cost2">Cost:1</div>
+        <div className="cost3">Cost:3</div>
+        <div className="cost4">Cost:2</div>
+        <div className="item1"type="button" onClick={itemClick1}></div>
+        <div className="item2"type="button" onClick={itemClick2}></div>
+        <div className="item3"type="button" onClick={itemClick3}></div>
+        <div className="item4"type="button" onClick={itemClick4}></div>
       </div>
     </div>
   );
